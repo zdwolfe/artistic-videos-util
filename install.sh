951c354b3a104704f7e9dcf0cb6e4692e75cdb91
zdwolfe <wolfe.zach@gmail.com>
2016-09-26 19:50:15 -0700

    install deps

--- install.sh
+++ install.sh
@@ -30,9 +30,9 @@ sudo ./configure
 sudo make
 sudo make install
 
-curl -s https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | bash
 git clone https://github.com/torch/distro.git ~/torch --recursive
-cd ~/torch; ./install.sh -b
+bash ~/torch/install-deps
+~/torch/install.sh -b
 source ~/.bashrc
 
 echo "Installing protobuf."
